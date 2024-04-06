2c1033afa7f01be811e7e88ade374bf91939854d
Kevin Da Cruz <kruzeraptor@gmail.com>
2024-04-06 14:44:38 +0100

    Added step cooldown

--- Assets/Scripts/PlayerDirector.cs
+++ Assets/Scripts/PlayerDirector.cs
@@ -10,6 +10,14 @@ public class PlayerDirector : MonoBehaviour
 {
     public static event Action OnPlayerMoveForwards;
 
+    private const string StepAnimationId = "Step";
+
+    [SerializeField] private Animator m_playerAnimator;
+    [SerializeField] private float m_forwardsCooldown = 0.5f;
+
+    private readonly int m_stepAnimationHashId = Animator.StringToHash(StepAnimationId);
+    private float m_forwardsCooldownTimer;
+
     private void OnDestroy()
     {
         OnPlayerMoveForwards = null;
@@ -17,10 +25,16 @@ public class PlayerDirector : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && (m_forwardsCooldownTimer == 0f))
         {
-            Debug.Log("Moved forwards.");
             OnPlayerMoveForwards?.Invoke();
+            m_playerAnimator.SetTrigger(m_stepAnimationHashId);
+            m_forwardsCooldownTimer = m_forwardsCooldown;
+        }
+
+        if (m_forwardsCooldownTimer > 0f)
+        {
+            m_forwardsCooldownTimer = Mathf.Clamp(m_forwardsCooldownTimer - Time.deltaTime, 0f, Mathf.Infinity);
         }
     }
 }
