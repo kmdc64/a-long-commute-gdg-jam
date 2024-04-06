b4865007b2bca8cb512b55ff1f7998951bb06676
Kevin Da Cruz <kruzeraptor@gmail.com>
2024-04-06 15:32:44 +0100

    Item generation & pickup system

---
+++ Assets/Scripts/SetPieceItem.cs
@@ -0,0 +1,29 @@
+/*
+ * SetPieceItem:
+ * A collectable item that is spawned on the track.
+ */
+
+using System;
+using UnityEngine;
+
+public class SetPieceItem : MonoBehaviour
+{
+    public enum ItemTypes
+    {
+        HappinessGain,
+        HappinessDrain,
+        Boost
+    }
+
+    public static event Action<SetPieceItem> OnAnyItemCollected;
+
+    public ItemTypes ItemType => m_itemType;
+
+    [SerializeField] private ItemTypes m_itemType;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnAnyItemCollected?.Invoke(this);
+        Destroy(gameObject);
+    }
+}
